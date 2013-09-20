f75c6b96ff42aba37dadc96d523e5c713e50a95e
Sinan Ugur Umu <suu13@biolz210-7.math.canterbury.ac.nz>
2013-09-20 20:30:23 +1200

    Update QC

--- scripts/antisense_expression.sh
+++ scripts/antisense_expression.sh
@@ -1,5 +1,5 @@
 
-echo -e "Species\tEMBL\tExperiment_ID\tDataset_ID\tStrand_Correlation\tCore_CDS_On\tCore_Rfam_On\tAnnot_Concordance\tCoverage\tFraction_mapped_reads"
+echo -e "Species\tEMBL\tExperiment_ID\tDataset_ID\tCore_CDS_On\tCore_Rfam_On"
 cat embl.directory.map.txt | while read i;
 do
 
@@ -20,11 +20,11 @@ cd plot/$species_directory;
 
 		#Correlation=$(echo `cat $plot | Rscript -e 'f=read.table("stdin"); cor(f[,1],f[,2]);'` | awk '{print $2}');
 		if [ ! -f $plot.Antisense.HMM_Core.expression ]; then gff_plot2median.py -g /home/suu13/projects/antisense/core_fasta/$embl_name*/$embl_name.HMM_Core.gff -p $plot -a 1> $plot.Antisense.HMM_Core.expression 2> /dev/null; fi;
-		if [ ! $plot.Antisense-rfam-filtered.expression ]; then gff_plot2median.py -g /home/suu13/projects/antisense/core_fasta/$embl_name*/$embl_name-rfam-filtered.gff -p $plot -a 1> $plot.Antisense-rfam-filtered.expression 2> /dev/null; fi;
+		if [ ! -f $plot.Antisense-rfam-filtered.expression ]; then gff_plot2median.py -g /home/suu13/projects/antisense/core_fasta/$embl_name*/$embl_name-rfam-filtered.gff -p $plot -a 1> $plot.Antisense-rfam-filtered.expression 2> /dev/null; fi;
 
-		#Core_CDS=$(cat $plot.HMM_Core.expression | awk 'BEGIN{FS="$"; total_c=0; treshold_c=0;}{total_c++; if($7>1) treshold_c++;}END{print treshold_c/total_c}');
-		#Core_Rfam=$(cat $plot.Rfam.expression | awk 'BEGIN{FS="$"; total_c=0; treshold_c=0;}{total_c++; if($7>1) treshold_c++;}END{print treshold_c/total_c}');
-		#echo -e "$species\t$embl_name\t$experiment_id\t$dataset_id\t$Correlation\t$Core_CDS\t$Core_Rfam\t$Concordance\t$Coverage_mapped_reads";
+		Core_CDS=$(cat $plot.Antisense.HMM_Core.expression | awk 'BEGIN{FS="$"; total_c=0; treshold_c=0;}{total_c++; if($7>1) treshold_c++;}END{print treshold_c/total_c}');
+		Core_Rfam=$(cat $plot.Antisense-rfam-filtered.expression | awk 'BEGIN{FS="$"; total_c=0; treshold_c=0;}{total_c++; if($7>1) treshold_c++;}END{print treshold_c/total_c}');
+		echo -e "$species\t$embl_name\t$experiment_id\t$dataset_id\t$Core_CDS\t$Core_Rfam";
 	fi;
 	done
 cd /home/suu13/projects/areba/Quality_Score;
